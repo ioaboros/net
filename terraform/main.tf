e8bfbcb9ff9955dfb06e6690d0f791e27f352aef
Panagiotis Pantelakis <panagiotis.pantelakis@b2badmin.org>
2024-10-24 11:41:17 +0300

    added bridge vlan conf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,14 @@ resource "mikrotik_bridge" "bridge" {
   vlan_filtering = false
   comment        = "Default bridge"
 }
+
+resource "mikrotik_bridge" "default" {
+  name = "main"
+}
+
+resource "mikrotik_bridge_vlan" "testacc" {
+  bridge   = mikrotik_bridge.default.name
+  tagged   = ["ether2", "vlan30"]
+  untagged = ["ether3"]
+  vlan_ids = [10, 30]
+}
